dbf52f81be65ebc07371c69241153ed58a6fbb31
Shriman Nijagun <EVOLVUS\shrimank@ELPBLR-025.evolvus.com>
2018-06-27 14:11:22 +0530

    Kafka Node added,consumer created.

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 const debug = require("debug")("evolvus-docket-client:index");
-const axios=require("axios");
+const axios = require("axios");
+const consumer = require('./torrent/kafka-config');
 
-var TIME_OUT=process.env.TIME_OUT || 5000;
+var TIME_OUT = process.env.TIME_OUT || 5000;
 
-module.exports.postToDocket=(docketObject)=> {
-    
-    return new Promise((resolve,reject)=> {
-        try {
-            var DOCKET_POST_URL=process.env.DOCKET_POST_URL || "http://localhost:3000/audit";
-            if(docketObject==null) {
-                debug(`IllegalArgument:Object is ${docketObject}`);
-                resolve(`IllegalArgument:Object is ${docketObject}`);
-            }
-            var instance = axios.create({
-                baseURL: DOCKET_POST_URL,
-                timeout: TIME_OUT
-            });
-            
-            instance.post(DOCKET_POST_URL,docketObject).then((response)=> {
-                debug("response is",response.data);
-                resolve(response.data);
-            }).catch((error)=> { 
-                debug(`Error:${error} and audit failed to store is`,docketObject);
-                resolve(docketObject);
-            });
-        } catch (error) {
-            debug(`caught exception ${error} and audit failed to store is`,docketObject);
-            resolve(error);
+consumer.on('message', function(message) {
+    console.log('On Receiveing ', message);
+    try {
+        const docketObject = JSON.parse(message.value);
+        var DOCKET_POST_URL = process.env.DOCKET_POST_URL || "http://localhost:3000/audit";
+        if (docketObject == null) {
+            debug(`IllegalArgument:Object is ${docketObject}`);
+            resolve(`IllegalArgument:Object is ${docketObject}`);
         }
-    });
-};
+        var instance = axios.create({
+            baseURL: DOCKET_POST_URL,
+            timeout: TIME_OUT
+        });
+
+        instance.post(DOCKET_POST_URL, docketObject).then((response) => {
+            debug("response is", response.data);
+
+        }).catch((error) => {
+            debug(`Error:${error} and audit failed to store is`, docketObject);
+
+        });
+    } catch (error) {
+        debug(`caught exception ${error} and audit failed to store is`, docketObject);
+    }
+});
+
+
+// module.exports.postToDocket = (docketObject) => {
+
+//     return new Promise((resolve, reject) => {
+//         try {
+//             var DOCKET_POST_URL = process.env.DOCKET_POST_URL || "http://localhost:3000/audit";
+//             if (docketObject == null) {
+//                 debug(`IllegalArgument:Object is ${docketObject}`);
+//                 resolve(`IllegalArgument:Object is ${docketObject}`);
+//             }
+//             var instance = axios.create({
+//                 baseURL: DOCKET_POST_URL,
+//                 timeout: TIME_OUT
+//             });
+
+//             instance.post(DOCKET_POST_URL, docketObject).then((response) => {
+//                 debug("response is", response.data);
+//                 resolve(response.data);
+//             }).catch((error) => {
+//                 debug(`Error:${error} and audit failed to store is`, docketObject);
+//                 resolve(docketObject);
+//             });
+//         } catch (error) {
+//             debug(`caught exception ${error} and audit failed to store is`, docketObject);
+//             resolve(error);
+//         }
+//     });
+// };
---
+++ torrent/kafka-config.js
@@ -0,0 +1,19 @@
+const KAFKA_BROKERS = process.env.KAFKA_BROKERS || 'localhost:9092';
+const TOPIC_NAME = process.env.AUDIT_EVENT_TOPIC || 'topic.audit.events';
+const PARTITIONS = process.env.PARTITIONS || '1';
+
+var kafka = require('kafka-node'),
+    client = new kafka.KafkaClient({ kafkaHost: KAFKA_BROKERS }),
+    Consumer = kafka.Consumer;
+
+
+var config = []
+for (var index = 0; index < parseInt(PARTITIONS); index++) {
+    config.push({ topic: TOPIC_NAME, partition: PARTITIONS });
+}
+
+module.exports.consumer = new Consumer(
+    client, config, {
+        autoCommit: true
+    }
+);
